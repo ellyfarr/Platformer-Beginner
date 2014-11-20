c99a23f68edb5bc28b31defe7ce45beed11ce013
Ellis Farr <farre@purdue.edu>
2014-11-19 19:05:55 -0500

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,18 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+
+public class Game {
+	public static void main (String[] agrs) {
+		JFrame frame = new JFrame("Platformer");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setLayout(new BorderLayout());
+		frame.add(new GamePanel(), BorderLayout.CENTER);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+	
+}
